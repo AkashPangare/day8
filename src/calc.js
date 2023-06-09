6315264292e0b42c83c9d52781686aa1aae10b64
Akash Pangare <apangare70@gmail.com>
2023-06-09 14:23:34 +0530

    function

--- src/calc.js
+++ src/calc.js
@@ -2,4 +2,8 @@ function sum(n1,n2){
 return n1+n2;
 }
 
-export{sum};
+function substract(n1,n2){
+    return n1-n2;
+}
+
+export{sum , substract};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import{sum} from "./calc.js";
+import{sum, substract} from "./calc.js";
 
 let output =sum(10,20);
 console.log(output);
+
+let output1=substract(50,10);
+console.log(output1);
